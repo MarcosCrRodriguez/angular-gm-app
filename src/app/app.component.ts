96fc50d53d32857bf7c9d382818ecda33d5f5266
MarcosCrRodriguez <programmer.crod@gmail.com>
2024-09-09 19:08:01 -0300

    menu desplegable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { LoginComponent } from './components/login/login.component';
 import { CommonModule } from '@angular/common';
 import { AuthService } from './services/auth.service';
@@ -22,23 +22,44 @@ import {
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app-angular-gm';
-
+  menu: string = 'home';
+  isMenuOpen = false;
   usuarioLogueado: any = null;
+
+  @ViewChild('menuRef', { static: true }) menuRef!: ElementRef;
 
   constructor(private authService: AuthService, private router: Router) {}
 
-  // se llama después de que Angular ha inicializado las propiedades de entrada del componente
   ngOnInit(): void {
     this.authService.usuarioLogueado$.subscribe((usuario) => {
-      console.log('Usuario logueado:', usuario.username);
+      if (usuario) {
+        console.log('Usuario logueado:', usuario.username);
+      }
       this.usuarioLogueado = usuario;
     });
+
+    // Comprobar el tamaño de la ventana al inicializar el componente
+    if (window.innerWidth > 768) {
+      this.isMenuOpen = false;
+    }
   }
 
   logout() {
     this.authService.logout();
     this.router.navigate(['/login']);
+  }
+
+  openMenu() {
+    this.isMenuOpen = true;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+
+  setMenu(menu: string) {
+    this.menu = menu;
   }
 }
