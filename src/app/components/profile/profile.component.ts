6809793e09c020ec064424a2684f87d3ca451496
MarcosCrRodriguez <programmer.crod@gmail.com>
2024-09-09 15:31:00 -0300

    cambio font

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrl: './profile.component.css',
 })
 export class ProfileComponent {
+  usuarioLogueado: any = null;
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    // nos suscribimos al observable para obtener el usuario logueado
+    this.authService.usuarioLogueado$.subscribe((usuario) => {
+      console.log('Usuario logueado en ProfileComponent:', usuario);
+      this.usuarioLogueado = usuario;
+    });
+  }
 }
