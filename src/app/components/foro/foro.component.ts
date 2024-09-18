675ae36f7116e316b6048798d0aa650789b13a63
MarcosCrRodriguez <programmer.crod@gmail.com>
2024-09-18 15:46:33 -0300

    finish-foro

--- src/app/components/foro/foro.component.ts
+++ src/app/components/foro/foro.component.ts
@@ -12,6 +12,7 @@ import { Observable } from 'rxjs';
 import { AuthService } from '../../services/auth.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import Toastify from 'toastify-js';
 
 @Component({
   selector: 'app-foro',
@@ -27,6 +28,7 @@ export class ForoComponent implements OnInit {
   public isLoadingMensajes: boolean = true;
   public usuarioAdmin: string = 'piedecamello@gmail.com';
   public usuarioEspecial: string = 'berrueberru@gmail.com';
+  public usuarioSlayer: string = 'doomslayer@hothell.com';
 
   constructor(private firestore: Firestore, private authService: AuthService) {}
 
@@ -48,6 +50,19 @@ export class ForoComponent implements OnInit {
   async enviarMensaje(): Promise<void> {
     const usuario = this.authService.getUsuarioLogueado();
     const mensaje = this.nuevoMensaje.trim();
+
+    if (mensaje.length > 250) {
+      // Muestra mensaje de error con Toastify
+      Toastify({
+        text: 'Demasiados caracteres. ¡Máximo 250!',
+        duration: 4000,
+        close: true,
+        gravity: 'top',
+        position: 'center',
+        backgroundColor: 'linear-gradient(to right, #ff5f6d, #ffc371)',
+      }).showToast();
+      return;
+    }
 
     if (usuario && mensaje) {
       const mensajesRef = collection(this.firestore, 'mensajes');
