0da42d027095b4ceeab5b1fb1c7f1134d0e1d208
MarcosCrRodriguez <programmer.crod@gmail.com>
2024-09-18 02:24:55 -0300

    new-comp

---
+++ src/app/components/foro/foro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-foro',
+  standalone: true,
+  imports: [],
+  templateUrl: './foro.component.html',
+  styleUrl: './foro.component.css',
+})
+export class ForoComponent {}
