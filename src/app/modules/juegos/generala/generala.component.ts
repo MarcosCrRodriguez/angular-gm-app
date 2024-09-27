4ab5b8cf745f5c07418fde306de9a9efaf3de906
MarcosCrRodriguez <programmer.crod@gmail.com>
2024-09-26 21:15:12 -0300

    updt-comp

--- src/app/modules/juegos/generala/generala.component.ts
+++ src/app/modules/juegos/generala/generala.component.ts
@@ -395,7 +395,7 @@ export class GeneralaComponent implements OnInit {
   }
 
   verificarFinDelJuego() {
-    if (this.contJugadasJugador === 8 && this.contJugadasIA === 8) {
+    if (this.contJugadasJugador === 0 && this.contJugadasIA === 0) {
       this.juegoIniciado = false;
       this.juegoTerminado = true;
       if (this.ptosJugador > this.ptosIA) {
