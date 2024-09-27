1435a969e54173ac14e2754dc863fc1ca9a2da7d
MarcosCrRodriguez <programmer.crod@gmail.com>
2024-09-26 21:31:37 -0300

    updt-comp

--- src/app/components/quien-soy/quien-soy.component.ts
+++ src/app/components/quien-soy/quien-soy.component.ts
@@ -1,14 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Cubilete } from '../../models/cubilete';
 
 @Component({
   selector: 'app-quien-soy',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './quien-soy.component.html',
   styleUrl: './quien-soy.component.css',
 })
 export class QuienSoyComponent implements OnInit {
   email: string = 'rodriguezmarcoscruz@gmail.com';
+  public cubilete: Cubilete = new Cubilete();
 
   ngOnInit() {
     window.scrollTo(0, 0);
