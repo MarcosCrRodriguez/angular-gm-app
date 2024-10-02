c517c71696b47b725ae0c6dafbf2dee635bf95c8
MarcosCrRodriguez <programmer.crod@gmail.com>
2024-10-01 22:46:24 -0300

    correccion-preguntados

--- src/app/services/banderas.service.ts
+++ src/app/services/banderas.service.ts
@@ -7,16 +7,39 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class BanderasService {
-  constructor(private http: HttpClient) {}
+  private nombresPaises: { [key: string]: string } = {}; // Almacena los nombres de los países
 
-  // Función para obtener países con datos filtrados
+  constructor(private http: HttpClient) {
+    this.cargarNombresPaises(); // Cargar los nombres de los países al inicializar el servicio
+  }
+
+  // Mapeo de continentes en inglés a español
+  private continentes: { [key: string]: string } = {
+    Europe: 'Europa',
+    Africa: 'África',
+    Americas: 'América',
+    Asia: 'Asia',
+    Oceania: 'Oceanía',
+    Antarctic: 'Antártida',
+  };
+
+  // Cargar nombres de países desde el archivo JSON
+  private cargarNombresPaises(): void {
+    this.http
+      .get<{ [key: string]: string }>('assets/paises.json')
+      .subscribe((data) => {
+        this.nombresPaises = data;
+      });
+  }
+
+  // Función para obtener países con nombres y continentes en español
   getPaises(): Observable<any[]> {
     return this.http.get<any[]>('https://restcountries.com/v3.1/all').pipe(
       map((paises) =>
         paises.map((pais) => ({
-          nombre: pais.name.common,
+          nombre: this.nombresPaises[pais.name.common] || pais.name.common, // Usar nombre en español
           bandera: pais.flags.png,
-          continente: pais.region,
+          continente: this.continentes[pais.region] || pais.region, // Traducir continente si es posible
           poblacion: pais.population,
         }))
       )
